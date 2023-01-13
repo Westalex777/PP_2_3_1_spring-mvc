7dd4a1b7c3ab1d91540368fb408eeec2a634529f
Westalex777 <westalex777@yandex.ru>
2023-01-13 19:09:35 +0600

    commit

--- src/main/java/com/pp231/controllers/UserControllers.java
+++ src/main/java/com/pp231/controllers/UserControllers.java
@@ -8,10 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @Controller
 public class UserControllers {
@@ -21,44 +18,35 @@ public class UserControllers {
 
     @GetMapping
     public String getAllUser(ModelMap model) {
-        List<User> users = userService.getAllUsers();
-        model.addAttribute("users", users);
+        model.addAttribute("users", userService.getAllUsers());
         return "index";
     }
 
-    @GetMapping("userForm")
-    public String userForm(Model model, @RequestParam(value = "id", required = false) int id) {
-        User user;
-        String toController;
-        String buttonName;
-        if (id == 0) {
-            user = new User();
-            toController = "/saveUser";
-            buttonName = "Create";
-        } else {
-            user = userService.getUser(id);
-            toController = "/updateUser";
-            buttonName = "Update";
-        }
-        model.addAttribute("toController", toController);
-        model.addAttribute("user", user);
-        model.addAttribute("buttonName", buttonName);
-        return "user-form";
+    @GetMapping("userCreate")
+    public String userCreate(Model model) {
+        model.addAttribute("user", new User());
+        return "user-create";
     }
 
-    @GetMapping("saveUser")
+    @GetMapping("addUser")
     public String saveUser(@ModelAttribute("user") User user) {
         userService.saveUser(user);
         return "redirect:/";
     }
 
-    @RequestMapping("deleteUser")
+    @GetMapping("deleteUser")
     public String deleteUser(@RequestParam("id") int id) {
         userService.deleteUser(id);
         return "redirect:/";
     }
 
-    @RequestMapping("updateUser")
+    @GetMapping("updateUserForm")
+    public String updateUserForm(@RequestParam("id") int id, Model model) {
+        model.addAttribute("user", userService.getUser(id));
+        return "user-update";
+    }
+
+    @GetMapping("updateUser")
     public String updateUser(@ModelAttribute("user") User user) {
         userService.updateUser(user);
         return "redirect:/";
